47ac4b0303a976fe7eb3b6fcf74c9808bfb5832c
Andika Hari Wijaya <hariwijayaandika@gmail.com>
2024-05-26 13:17:03 +0700

    init

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+
+    <h1>Halaman Blog</h1>
+    <link rel="stylesheet" href="css/style.css">
+    {{-- Contoh Artikel --}}
+    <h1>Belajar Laravel 11</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, optio suscipit quam error itaque autem aperiam distinctio consectetur et veniam quos.
+        Accusamus error sint quos, nemo minima deserunt at repudiandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, optio suscipit quam error itaque autem aperiam distinctio consectetur et veniam quos.
+        Accusamus error sint quos, nemo minima deserunt at repudiandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, optio suscipit quam error itaque autem aperiam distinctio consectetur et veniam quos.
+        Accusamus error sint quos, nemo minima deserunt at repudiandae.</p>
+    <h1>Latihan Laravel 11</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, optio suscipit quam error itaque autem aperiam distinctio consectetur et veniam quos.
+        Accusamus error sint quos, nemo minima deserunt at repudiandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, optio suscipit quam error itaque autem aperiam distinctio consectetur et veniam quos.
+        Accusamus error sint quos, nemo minima deserunt at repudiandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, optio suscipit quam error itaque autem aperiam distinctio consectetur et veniam quos.
+        Accusamus error sint quos, nemo minima deserunt at repudiandae.</p>
+</body>
+</html>
